63134a80f3ead69d1a1b70069497d5045abe49af
Kyle <khartson2017@gmail.com>
2022-05-05 14:47:00 -0400

    commit tree aa45ceda88b78c5458ed2f5385e7c4134368d879

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,27 @@
 def starts_with_a_vowel?(word)
-
+    return true if !word.scan(/^[aeiouAEIOU]/).empty?
+    return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    matches = []
+    text.split.each do |w|
+        matches.push(w) if w.match(/un.*ing$/)
+    end
+    return matches
 end
 
 def words_five_letters_long(text)
-
+    matches = []
+    text.split.each do |w|
+        matches.push(w) if w.match(/\A[a-z]{5}$/)
+    end
+    return matches
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    return true if text.match(/^[A-Z]{1}.*[!.?"]$/)
+    return false
 end
 
 def valid_phone_number?(phone)
